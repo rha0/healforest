caa438aafae8ad3bef0f5c635fa83cbf4a2fe70a
hayoung <yhy55045224@gmail.com>
2020-04-21 15:21:06 +0900

    치유의숲 화면

---
+++ source/js/common.js
@@ -0,0 +1,61 @@
+
+
+$(document).ready(function() {
+
+    $("th.number, td.number").click(function(){
+    $("input#number").val($("input#number").val()+$(this).html());
+    })
+
+    $(".delete").click(function(){
+        var inputText = $("input#number").val();
+       $("input#number").val(inputText.substr(0, inputText.length -1));
+  
+    })
+
+    $(".re-entry").click(function(){
+    $("input#number").val("");
+    })
+
+
+    $('a.print_btn').click(function() {
+        $("div.popup").css("display", "block");
+        $('div.popup').css("height", $(document).height());
+    });
+
+    $("div.more a").click(function() {
+        $(this).css("display", "none");
+        $("div.list_wrap").addClass("down");
+        
+        return false;
+    });
+
+    $("div#map p.img").each(function() {
+        $(this).css("background", "url('" + $(this).find("img").attr("src") + "') no-repeat top / cover");
+    });
+
+    $("div.ad").slick({
+        autoplay:true,
+        speed: 2000,
+        infinite: true,
+    });
+        
+    $("div.map_img").slick({
+        speed: 1000,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        asNavFor: ".guid",
+    }); 
+
+    $("div.guid").slick({
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        fade: true,
+        asNavFor: ".map_img",
+    });
+});
+
